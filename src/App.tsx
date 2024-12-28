8daea2ac7cc1e2cba56334761b90e5ca9c532426
Murat Dogan <murat.dogan13@outlook.com>
2024-12-28 11:06:28 +0300

    Add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './App.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './features/counter/counterSlice';
 
 function App() {
+  const count = useSelector((state: any) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
       <div className={styles.testClass}>
         Test
       </div>
+      <span>{count}</span>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
     </div>
   );
 }
