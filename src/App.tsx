aabaecb33c6af7ceb172382e54b623e8488e855c
Murat Dogan <murat.dogan13@outlook.com>
2024-12-28 10:33:56 +0300

    Add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
     <div className="App">
+      <div className={styles.testClass}>
+        Test
+      </div>
     </div>
   );
 }
