78dfe23a4b8b61d59e3dac96a4a3512a5710c2c1
Murat Dogan <murat.dogan13@outlook.com>
2024-12-29 14:11:13 +0300

    Remove unused slice

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice";
 import mediaSlice from "./features/media/mediaSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     media: mediaSlice,
   },
 });
